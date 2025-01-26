e92935a2a58081aaee5dd0c65f2cb767f7231b0b
irwanphan <irwanphan@gmail.com>
2025-01-27 02:32:57 +0700

    remove unused icon

--- app/@content/workspace-management/layout.tsx
+++ app/@content/workspace-management/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Star, Users, Building2, Network, FileCheck } from 'lucide-react';
+import { Star, Users, Building2, FileCheck } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
