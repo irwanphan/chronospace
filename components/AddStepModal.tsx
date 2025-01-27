cbb00b7c967c2ed5604723e2eb644aa9bd2196bc
irwanphan <irwanphan@gmail.com>
2025-01-27 15:15:27 +0700

    save schema

--- components/AddStepModal.tsx
+++ components/AddStepModal.tsx
@@ -188,7 +188,8 @@ export default function AddStepModal({
               Cancel
             </button>
             <button
-              type="submit"
+              type="button"
+              onClick={handleSubmit}
               className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
             >
               Add Step
