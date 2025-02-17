83812db401225b078f8096658b8b9554055dfb21
irwanphan <irwanphan@gmail.com>
2025-02-18 00:14:32 +0700

    reroute work division

--- app/(protected)/workspace-management/work-division/[id]/page.tsx
+++ app/(protected)/workspace-management/work-division/[id]/page.tsx
@@ -113,7 +113,7 @@ export default function EditWorkDivisionPage({ params }: { params: { id: string 
         <div className="flex justify-end gap-3">
           <button
             type="button"
-            onClick={() => router.back()}
+            onClick={() => router.push(`/workspace-management/work-division`)}
             className="px-4 py-2 border rounded-lg hover:bg-gray-50"
           >
             Back
