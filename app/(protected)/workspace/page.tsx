39cb7335ecb13179a71c9e0b6d1a54a1359e5db4
irwanphan <irwanphan@gmail.com>
2025-02-22 08:40:13 +0700

    add loading animation in workspace

--- app/(protected)/workspace/page.tsx
+++ app/(protected)/workspace/page.tsx
@@ -119,7 +119,11 @@ export default function WorkspacePage() {
   // console.log('Purchase Requests:', purchaseRequests);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center h-screen">
+        <div className="w-20 h-20 border-t-2 border-b-2 border-blue-900 rounded-full animate-spin"></div>
+      </div>
+    </div>;
   }
 
   return (
