e94170a27b51175ae08c28c1755dc4330c48f4e7
irwanphan <irwanphan@gmail.com>
2025-04-04 13:07:57 +0700

    Comment out unused imports in DocumentViewerPage to improve code clarity and maintainability.

--- app/(protected)/documents/view/page.tsx
+++ app/(protected)/documents/view/page.tsx
@@ -2,8 +2,6 @@
 
 import dynamic from 'next/dynamic';
 import LoadingSpin from '@/components/ui/LoadingSpin';
-// import CertificateStatus from '@/components/CertificateStatus';
-// import SignatureInfo from '@/components/SignatureInfo';
 
 // Import PDF viewer component secara dynamic dengan disabled SSR
 const PDFViewer = dynamic(
@@ -19,10 +17,7 @@ export default function DocumentViewerPage() {
   return (
     <div className="max-w-full">
       <div className="flex justify-between items-center">
-        {/* <h1 className="text-2xl font-semibold">Document Viewer</h1> */}
         <div className="flex items-center gap-4">
-          {/* <SignatureInfo /> */}
-          {/* <CertificateStatus /> */}
         </div>
       </div>
       <PDFViewer />
