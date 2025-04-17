6a9c85566fc65bccc46c8ddd3a7b1d9fe1f7d1fa
irwanphan <irwanphan@gmail.com>
2025-04-17 14:51:38 +0700

    Update user access seeder and workspace access control for purchase order permissions
    - Enhanced the user access seeder to include additional permissions related to purchase orders, ensuring comprehensive access control.
    - Updated workspace access control to reflect the new permissions structure, improving user role management and functionality coverage.

---
+++ prisma/seeders/user-administrator-seeder.ts
@@ -0,0 +1,105 @@
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcrypt";
+
+const password = "password";
+
+export async function userSeeder() {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const userData = [
+    { 
+      name: 'John CEO', 
+      email: 'magus@chronospace.id', 
+      password: hashedPassword, 
+      roleId: 'administrator', 
+      phone: '000', 
+      workDivisionId: 'RND', 
+      employeeId: 'CHR', 
+      residentId: 'Zeal000', 
+      nationality: 'ZL', 
+      isActive: true, 
+      birthday: new Date('1986-04-09') 
+    },
+  ];
+
+  const user = await prisma.user.create({
+    data: userData[0]
+  });
+
+  await prisma.role.create({
+    data: { 
+      roleCode: 'ADMIN',
+      roleName: 'Administrator',
+      description: 'Administrator',
+      upperLevel: null,
+      budgetLimit: 0,
+    },
+  });
+
+  await prisma.userRole.create({
+    data: {
+      userId: user.id,
+      roleId: 'administrator',
+    },
+  });
+
+  await prisma.userAccess.create({
+    data: {
+      userId: user.id,
+      menuAccess: {
+        timeline: true,
+        workspace: true,
+        projectPlanning: true,
+        budgetPlanning: true,
+        userManagement: true,
+        workspaceManagement: true,
+        documents: true,
+      },
+      activityAccess: { 
+        createProject: true,
+        editProject: true,
+        deleteProject: true,
+        createBudget: true,
+        editBudget: true,
+        deleteBudget: true,
+        createWorkDivision: true,
+        editWorkDivision: true,
+        deleteWorkDivision: true,
+        createRole: true,
+        editRole: true,
+        deleteRole: true,
+        createVendor: true,
+        editVendor: true,
+        deleteVendor: true,
+        createApprovalSchema: true,
+        editApprovalSchema: true,
+        deleteApprovalSchema: true,
+        createUser: true,
+        editUser: true,
+        deleteUser: true,
+        changePassword: true,
+        changeOtherUserPassword: true,
+        manageUserAccess: true,
+        createDocument: true,
+        uploadDocument: true,
+        deleteDocument: true,
+        downloadDocument: true,
+        generateCertificate: true,
+      },
+      workspaceAccess: {
+        createPurchaseRequest: true,
+        viewPurchaseRequest: true,
+        editPurchaseRequest: true,
+        reviewApprovePurchaseRequest: true,
+        viewOthersPurchaseRequest: true,
+        viewOtherDivisionPurchaseRequest: true,
+        createPurchaseOrder: true,
+        viewPurchaseOrder: true,
+        viewOthersPurchaseOrder: true,
+        viewOtherDivisionPurchaseOrder: true,
+        generatePurchaseOrderDocument: true,
+        signDocument: true,
+      },
+    },
+  });
+}
